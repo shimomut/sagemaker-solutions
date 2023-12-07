dabd0ba08f4c6e24b6fad4dbeef5d40251e8a271
Tomonori Shimomura <shimomut@amazon.com>
2023-12-07 19:56:45 +0000

    apply netplan and confirm ad reachable

--- hyperpod_sssd/configure_sssd.py
+++ hyperpod_sssd/configure_sssd.py
@@ -1,8 +1,13 @@
 import os
 import subprocess
 import tempfile
+import socket
 
 # ---
+
+# Configurations
+
+ad_domain = "cluster-test.amazonaws.com"
 
 # If this script is executed by root already, this variable can be empty
 sudo_command = ["sudo"]
@@ -16,8 +21,25 @@ netplan_filename_for_custom_dns = "/etc/netplan/99-custom-dns.yaml"
 network_interface_name = "ens6"
 dns_server_addresses = [ "10.3.73.85", "10.2.82.19" ]
 
+exit_on_failure = True
 
 # ---
+
+def on_failure(message):
+    print(message)
+    if exit_on_failure:
+        sys.exit(1)
+
+def install_apt_packages():
+
+    print("---")
+    print("Updating apt package index")
+    subprocess.run( [ *sudo_command, "apt", "update" ] )
+
+    print("---")
+    print("Installing packages - ", packages_to_install)
+    subprocess.run( [ *sudo_command, "apt", "install", "-y", *packages_to_install ] )
+
 
 netplan_custom_dns_yaml = f"""
 network:
@@ -30,20 +52,9 @@ network:
                 use-dns: false
 """
 
-# ---
-
-def install_apt_packages():
-
-    print("Updating apt package index")
-    subprocess.run( [ *sudo_command, "apt", "update" ] )
-
-    print("---")
-    print("Installing packages - ", packages_to_install)
-    subprocess.run( [ *sudo_command, "apt", "install", "-y", *packages_to_install ] )
-
-
 def configure_custom_dns():
 
+    print("---")
     print("Creating netplan config file for custom DNS server addresses")
 
     with tempfile.TemporaryDirectory() as tmp_dir:
@@ -55,10 +66,22 @@ def configure_custom_dns():
         subprocess.run( [ *sudo_command, "chown", "root:root", tmp_yaml_filename ] )
         subprocess.run( [ *sudo_command, "mv", tmp_yaml_filename, netplan_filename_for_custom_dns ] )
 
+    print("---")
+    print("Applying netplan change (warning about ens6 can be ignored)")
+    subprocess.run( [ *sudo_command, "netplan", "apply" ] )
+
+    print("---")
+    print("Confirming AD domain is reachable")
+    try:
+        address = socket.gethostbyname(ad_domain)
+        print( f"{ad_domain} -> {address}" )
+    except gaierror:
+        on_failure(f"Warning : {ad_domain} cannot be resolved")
+
 
 print("Starting SSSD configuration steps")
 
-install_apt_packages()
+#install_apt_packages()
 configure_custom_dns()
 
 print("---")
