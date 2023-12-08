2fc70740d160a987ece43120672393ffdae2ec20
Tomonori Shimomura <shimomut@amazon.com>
2023-12-08 06:19:18 +0000

    Checking GPU failure

--- hyperpod_resiliency/job.sh
+++ hyperpod_resiliency/job.sh
@@ -5,7 +5,10 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH -N 2
 
-srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --fail &
+#srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --raise-exception &
+#srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task2 &
+
+srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --gpu-failure &
 srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task2 &
 
 wait
--- hyperpod_resiliency/step.py
+++ hyperpod_resiliency/step.py
@@ -3,20 +3,47 @@
 import sys
 import os
 import time
+import subprocess
 import argparse
 
 argparser = argparse.ArgumentParser(description='Test slurm step')
 argparser.add_argument('--name', action='store', help='name of this task')
-argparser.add_argument('--fail', action='store_true', help='exit with return code 1')
+argparser.add_argument('--raise-exception', action='store_true', help='cause assetion failure')
+argparser.add_argument('--gpu-failure', action='store_true', help='Simulate gpu failure and exit with return code 1')
 args = argparser.parse_args()
 
 
-print(f"Running task [{args.name}]", flush=True)
+gpu_failure_flag_filename = "./gpu-already-failed.txt"
+
+
+print(f"[{args.name}] Running task", flush=True)
+
+#slurm_node_id = os.environ["SLURM_NODEID"]
+
+if 0:
+    print(f"[{args.name}] Slurm related environment variables:", flush=True)
+    for k in os.environ:
+        if k.startswith("SLURM"):
+            v = os.environ[k]
+            print(f"  {k} : {v}", flush=True)
 
 time.sleep(10)
 
-if args.fail:
-    print(f"Task [{args.name}] failed.", flush=True)
-    sys.exit(1)
+if args.gpu_failure:
 
-print(f"Task [{args.name}] finished.", flush=True)
+    if not os.path.exists(gpu_failure_flag_filename):
+
+        with open(gpu_failure_flag_filename,"w") as fd:
+            pass
+
+        print(f"[{args.name}] Simulating GPU failure.", flush=True)
+
+        # TODO : some code to simulate GPU failure
+        subprocess.run(["sudo", "python3", "simulate_gpu_failure.py"])
+
+        sys.exit(1)
+
+if args.raise_exception:
+    assert False, f"[{args.name}] Simulating task failure"
+
+print(f"[{args.name}] Finished.", flush=True)
