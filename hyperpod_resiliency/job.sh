fb01225d6b70b753cc88569233508934124c6bed
Tomonori Shimomura <shimomut@amazon.com>
2023-12-10 18:10:29 +0000

    Auto-resume succeeds

--- hyperpod_resiliency/job.sh
+++ hyperpod_resiliency/job.sh
@@ -3,9 +3,20 @@
 #SBATCH -o ./output/log_%j.txt
 #SBATCH --time=1:00:00
 #SBATCH --ntasks-per-node 1
-#SBATCH -N 2
+#SBATCH -N 6
+#SBATCH --exclusive
 
-srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --fail &
-srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task2 &
+#srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --raise-exception &
+#srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task2 &
 
-wait
+#srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --gpu-failure &
+#srun --nodes 1 --ntasks 1 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task2 &
+
+#srun --nodes 4 --ntasks 4 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 --gpu-failure &
+
+#srun --nodes 6 --cpus-per-task=1 --auto-resume=1 python3 step.py --name task1 &
+
+# Auto resume succeeds
+srun --nodes 6 --auto-resume=1 python3 step.py --name task1 --gpu-failure 
+
+
