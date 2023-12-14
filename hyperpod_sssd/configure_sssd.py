f63a90d317166ac680067478d0cfb4304ce78b37
Tomonori Shimomura <shimomut@amazon.com>
2023-12-14 03:37:17 +0000

    End to end with ad provider

--- hyperpod_sssd/configure_sssd.py
+++ hyperpod_sssd/configure_sssd.py
@@ -1,4 +1,5 @@
 import os
+import time
 import subprocess
 import tempfile
 import socket
@@ -17,12 +18,12 @@ sudo_command = ["sudo","-E"]
 #sudo_command = []
 
 packages_to_install = [
-    "ldap-utils", "sssd-tools", "adcli", "sssd",
-    "libnss-sss", "libpam-sss",
-    "sssd-ldap",
-    "sssd-dbus",
-    #"realmd", 
-    #"sssd-krb5", "krb5-user" # not needed ?
+    "sssd",
+    "ldap-utils",
+    "sssd-tools",
+    "sssd-krb5",
+    "krb5-user",
+    "realmd",
 ]
 
 netplan_filename_for_custom_dns = "/etc/netplan/99-custom-dns.yaml"
@@ -38,7 +39,7 @@ sssd_config_filename = "/etc/sssd/sssd.conf"
 
 krb5_config_filename = "/etc/krb5.conf"
 
-ad_admin_password = "xyz" # FIXME : read from Secrets Manager?
+ad_admin_password = {placeholder} # FIXME : read from Secrets Manager?
 
 # ---
 
@@ -82,21 +83,20 @@ def configure_custom_dns():
     print("Applying netplan change (warning about ens6 can be ignored)")
     subprocess.run( [ *sudo_command, "netplan", "apply" ] )
 
+    # It takes some time until when host name can be resolved
+    time.sleep(10)
+
     print("---")
     print("Confirming AD domain is reachable")
-    address = socket.gethostbyname(ad_domain)
+    max_retries = 10
+    for i in range(max_retries):
+        try:
+            print(f"Attempt {i+1} / {max_retries}")
+            address = socket.gethostbyname(ad_domain)
+            break
+        except socket.gaierror:
+            time.sleep(10)
     print( f"{ad_domain} -> {address}" )
-
-
-# FIXME : This shouldn't be needed
-def realm_join():
-    # FIXME : should print stdout/stderr
-    p = pexpect.popen_spawn.PopenSpawn([*sudo_command, "realm", "join", "-U", "Admin", ad_domain])
-    p.expect(":")
-    p.sendline(ad_admin_password)
-    result = p.wait()
-    print(result)
-    assert result==0, "realm join failed."
 
 
 def enable_password_authentication():
@@ -128,43 +128,6 @@ def enable_automatic_homedir_creation():
     subprocess.run( [ *sudo_command, "pam-auth-update", "--enable", "mkhomedir" ] )
 
 
-sssd_conf = f"""
-[sssd]
-domains = {ad_domain}
-config_file_version = 2
-services = nss, pam
-
-[domain/{ad_domain}]
-default_shell = /bin/bash
-krb5_store_password_if_offline = True
-cache_credentials = True
-krb5_realm = {ad_domain.upper()}
-realmd_tags = manages-system joined-with-adcli
-id_provider = ad
-fallback_homedir = /home/%u@%d
-ad_domain = {ad_domain}
-use_fully_qualified_names = True
-ldap_id_mapping = True
-access_provider = ad
-"""
-
-# FIXME : not needed if what realm join generated is sufficient
-def configure_sssd():
-
-    print("---")
-    print("Configuring SSSD")
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_sssd_config_filename = os.path.join(tmp_dir, os.path.basename(sssd_config_filename))
-        with open(tmp_sssd_config_filename,"w") as fd:
-            fd.write( sssd_conf.strip() )
-
-        subprocess.run( [ *sudo_command, "chmod", "600", tmp_sssd_config_filename ] )
-        subprocess.run( [ *sudo_command, "chown", "root:root", tmp_sssd_config_filename ] )
-        subprocess.run( [ *sudo_command, "cp", tmp_sssd_config_filename, sssd_config_filename ] )
-
-
-# FIXME : not needed ?
 def configure_krb5():
 
     print("---")
@@ -187,6 +150,65 @@ def configure_krb5():
         subprocess.run( [ *sudo_command, "cp", tmp_config_filename, krb5_config_filename ] )
 
 
+def realm_join():
+
+    print("---")
+    print("Joining AD")
+
+    max_retries = 10
+    for i in range(max_retries):
+
+        print(f"Attempt {i+1} / {max_retries}")
+
+        p = pexpect.popen_spawn.PopenSpawn([*sudo_command, "realm", "join", "-U", "Admin", ad_domain])
+        p.expect(":", timeout=30)
+        print(p.before.decode("utf-8") + p.after.decode("utf-8"), end="")
+        p.sendline(ad_admin_password)
+        p.expect(pexpect.EOF, timeout=30)
+        print(p.before.decode("utf-8"), end="")
+        result = p.wait()
+        if result==0: break
+
+        time.sleep(10)
+    else:
+        assert result==0, f"Joining AD domain failed with return code {result}"
+
+
+sssd_conf = f"""
+[sssd]
+domains = {ad_domain}
+config_file_version = 2
+services = nss, pam
+
+[domain/{ad_domain}]
+default_shell = /bin/bash
+krb5_store_password_if_offline = True
+cache_credentials = True
+krb5_realm = {ad_domain.upper()}
+realmd_tags = manages-system joined-with-adcli
+id_provider = ad
+fallback_homedir = /home/%u@%d
+ad_domain = {ad_domain}
+use_fully_qualified_names = True
+ldap_id_mapping = True
+access_provider = ad
+"""
+
+def configure_sssd():
+
+    print("---")
+    print("Configuring SSSD")
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_sssd_config_filename = os.path.join(tmp_dir, os.path.basename(sssd_config_filename))
+        with open(tmp_sssd_config_filename,"w") as fd:
+            fd.write( sssd_conf.strip() )
+
+        subprocess.run( [ *sudo_command, "chmod", "600", tmp_sssd_config_filename ] )
+        subprocess.run( [ *sudo_command, "chown", "root:root", tmp_sssd_config_filename ] )
+        subprocess.run( [ *sudo_command, "cp", tmp_sssd_config_filename, sssd_config_filename ] )
+
+
 def restart_services():
 
     print("---")
@@ -198,14 +220,14 @@ def restart_services():
 
 print("Starting SSSD configuration steps")
 
+install_apt_packages()
 configure_custom_dns()
-#install_apt_packages()
-#realm_join()
-#enable_password_authentication()
-#enable_automatic_homedir_creation()
-#restart_services()
-#configure_sssd()
-#configure_krb5()
+enable_password_authentication()
+enable_automatic_homedir_creation()
+configure_krb5()
+realm_join()
+configure_sssd()
+restart_services()
 
 print("---")
 print("Finished SSSD configuration steps")
