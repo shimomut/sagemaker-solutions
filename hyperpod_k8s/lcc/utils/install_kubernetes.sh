4cb0965b5123bf1f89417e8a1e72356f55e0ee9b
Tomonori Shimomura <shimomut@amazon.com>
2024-02-01 22:29:48 +0000

    automatically overwrite if exist

--- hyperpod_k8s/lcc/utils/install_kubernetes.sh
+++ hyperpod_k8s/lcc/utils/install_kubernetes.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 sudo install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
