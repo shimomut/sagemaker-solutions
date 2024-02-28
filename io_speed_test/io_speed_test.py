f06e658ca8667e2391caee01c8a2a7d053d121b2
Tomonori Shimomura <shimomut@amazon.com>
2024-02-28 22:09:01 +0000

    Use NVMe for tmp. Use file size in the file names.

--- io_speed_test/io_speed_test.py
+++ io_speed_test/io_speed_test.py
@@ -15,6 +15,7 @@ class Config:
 
     s3_location = "s3://shimomut-files-vpce-us-east-2-842413447717/tmp/"
     fsx_location = "/fsx/ubuntu/tmp"
+    tmp_location = "/opt/dlami/nvme"
 
     #concurrent_executor = "thread"
     concurrent_executor = "process"
@@ -32,7 +33,7 @@ class Config:
         num_files = 10
         max_workers = 10
         s3_transfer_config = None
-    elif 1:
+    elif 0:
         file_size = 1024 * 1024 * 1024 # 1 GB
         num_files = 10
         max_workers = 1
@@ -81,6 +82,24 @@ class Config:
             multipart_threshold = 10 * 1024 * 1024,
             multipart_chunksize = 10 * 1024 * 1024
             )
+    elif 1:
+        file_size = 50 * 1024 * 1024 * 1024 # 50 GB
+        num_files = 1
+        max_workers = 1
+        s3_transfer_config = None
+
+    @staticmethod
+    def print():
+
+        print(f"s3_location : {Config.s3_location}")
+        print(f"fsx_location : {Config.fsx_location}")
+        print(f"tmp_location : {Config.tmp_location}")
+        print(f"concurrent_executor : {Config.concurrent_executor}")
+        print(f"s5cmd_concurrency : {Config.s5cmd_concurrency}")
+        print(f"file_size : {Config.file_size}")
+        print(f"num_files : {Config.num_files}")
+        print(f"max_workers : {Config.max_workers}")
+        print(f"s3_transfer_config : {Config.s3_transfer_config}")
 
 
 def split_s3_path( s3_path ):
@@ -90,6 +109,15 @@ def split_s3_path( s3_path ):
     key = re_result.group(2)
     key = key.rstrip("/")
     return bucket, key
+
+
+def get_file_size_string(size):
+
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
+        if size < 1024 or unit=='PB':
+            break
+        size //= 1024
+    return f"{size}{unit}"
 
 
 class App:
@@ -168,7 +196,7 @@ class App:
 
         print(f"Downloading {s3_path} with AWSCLI(CRT)")
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
+        with tempfile.TemporaryDirectory(dir=Config.tmp_location) as tmp_dir:
             tmp_filename = os.path.join(tmp_dir, os.path.basename(s3_path))
             subprocess.run(["aws", "s3", "cp", s3_path, tmp_filename ])
 
@@ -178,7 +206,7 @@ class App:
 
         print(f"Downloading {s3_path} with s5cmd")
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
+        with tempfile.TemporaryDirectory(dir=Config.tmp_location) as tmp_dir:
             tmp_filename = os.path.join(tmp_dir, os.path.basename(s3_path))
             subprocess.run(["s5cmd", "cp", "--concurrency", f"{Config.s5cmd_concurrency}", s3_path, tmp_filename ])
 
@@ -211,6 +239,8 @@ class App:
 
     def main(self):
 
+        Config.print()
+
         print("Creating random bytes")
         src_buffer = io.BytesIO()
         size_wrote = 0
@@ -220,8 +250,9 @@ class App:
             size_wrote += src_buffer.write( random.randbytes(size_to_write) )
 
     
-        s3_paths = [ Config.s3_location + "large_%04d.bin" % i for i in range(Config.num_files) ]
-        fsx_paths = [ os.path.join( Config.fsx_location, ("large_%04d.bin" % i) ) for i in range(Config.num_files) ]
+        prefix_file_size = get_file_size_string(Config.file_size)
+        s3_paths = [ Config.s3_location + f"{prefix_file_size}_{i:04d}.bin" for i in range(Config.num_files) ]
+        fsx_paths = [ os.path.join( Config.fsx_location, f"{prefix_file_size}_{i:04d}.bin" ) for i in range(Config.num_files) ]
 
         os.makedirs(Config.fsx_location, exist_ok=True)
 
