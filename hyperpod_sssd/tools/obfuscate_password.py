a4c99f1058558c874e3d4a812f35d54a918850f5
Tomonori Shimomura <shimomut@amazon.com>
2024-03-09 00:01:28 +0000

    Make more robust

--- hyperpod_sssd/tools/obfuscate_password.py
+++ hyperpod_sssd/tools/obfuscate_password.py
@@ -1,6 +1,12 @@
+import sys
 import getpass
+
 import pysss
 
-admin_pw = getpass.getpass("AD Admin password: ")
+
+admin_pw = getpass.getpass("AD Admin password: ").strip()
+if not admin_pw:
+    print("Invalid password")
+    sys.exit(1)
 
 print(pysss.password().encrypt(admin_pw, pysss.password().AES_256))
