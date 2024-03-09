5ad2623f923d2390247a539cb79ad7b96d18d3b8
Tomonori Shimomura <shimomut@amazon.com>
2024-03-08 17:28:21 -0800

    Let entire script fail when sssd failed

--- hyperpod_sssd/lcc/lifecycle_script.py
+++ hyperpod_sssd/lcc/lifecycle_script.py
@@ -170,7 +170,7 @@ def main(args):
         #     ExecuteBashScript("./utils/install_prometheus.sh").run()
 
         # Enable AcriveDirectory/LDAP integration
-        subprocess.run(["python3", "-u", "configure_sssd.py"])
+        subprocess.run(["python3", "-u", "configure_sssd.py"], check=True)
 
     print("[INFO]: Success: All provisioning scripts completed")
 
