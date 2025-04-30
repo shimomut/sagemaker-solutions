1d75135259a7692dc4530c0318c2248432715fcf
Tomonori Shimomura <shimomut@amazon.com>
2025-04-30 18:06:58 +0000

    Call webhook for all API kinds to understand the behavior

--- _experiments/hyperpod_eks_dynamic_admission/webhook.py
+++ _experiments/hyperpod_eks_dynamic_admission/webhook.py
@@ -25,9 +25,17 @@ class APIHandler(BaseHTTPRequestHandler):
 
     def _validate(self, req):
 
+        kind = req["kind"]
         operation = req["operation"]
         api_invoker_username = req["userInfo"]["username"]
         api_invoker_groups = req["userInfo"]["groups"]
+
+        print(f"Operation: {operation}, Kind: {kind}")
+
+        # Allow unknown kinds
+        if kind["kind"] not in ["Pod", "Deployment"]:
+            print("Ignoring unknown kinds:", kind["kind"])
+            return True, 200, ""
 
         # Allow all groups that start with "system:" prefix (except for system:authenticated).
         # Allow "dynamic-admission:admin" group.
@@ -35,11 +43,12 @@ class APIHandler(BaseHTTPRequestHandler):
             if group in ["system:authenticated"]:
                 continue
             if group.startswith("system:") or group == "dynamic-admission:admin":
-                print("Validated by group:", group)
+                print(f"Allowing by group: {group}")
                 return True, 200, ""
 
         # Allow all resource creation operations.
         if operation in ["CREATE"]:
+            print(f"Allowing operation {operation}")
             return True, 200, ""
 
         # Get target object information
@@ -56,11 +65,13 @@ class APIHandler(BaseHTTPRequestHandler):
 
         # If owner information is missing in the object, allow everything
         if obj_owner is None:
+            print(f"Allowing {operation} operation to resources without dynamic admission owner")
             return True, 200, ""
 
         if obj_owner != api_invoker_username:
             return False, 403, f"Forbidden access to resource owned by different user (owner:{obj_owner} != you:{api_invoker_username})"
 
+        print(f"Allowing {operation} operation to resources owned by the API invoker ({api_invoker_username})")
         return True, 200, ""
 
     def do_POST(self):
@@ -72,8 +83,8 @@ class APIHandler(BaseHTTPRequestHandler):
             try:
                 post_data = self._read_post_data()
 
-                print("Request:")
-                print(json.dumps(post_data, indent=2))
+                # print("Request:")
+                # print(json.dumps(post_data, indent=2))
 
                 request_id = post_data["request"]["uid"]
 
@@ -102,8 +113,8 @@ class APIHandler(BaseHTTPRequestHandler):
                         }
                     }
 
-                print("Response:")
-                print(json.dumps(response_data, indent=2))
+                # print("Response:")
+                # print(json.dumps(response_data, indent=2))
 
                 self._send_response(response_data)
 
