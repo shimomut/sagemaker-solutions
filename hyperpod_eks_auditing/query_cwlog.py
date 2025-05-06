95bf88184c3506009c9b8815377c7f7acc366958
Tomonori Shimomura <shimomut@amazon.com>
2025-05-06 22:42:39 +0000

    Read all streams and print interesting events only

--- hyperpod_eks_auditing/query_cwlog.py
+++ hyperpod_eks_auditing/query_cwlog.py
@@ -1,13 +1,78 @@
 
-import time
+import sys
 import json
+import fnmatch
+import re
+import datetime
+
 import boto3
 
-print_malformed_json = True
-
 region_name = "us-west-2"
-log_group_name = "/aws/eks/sagemaker-hyperpod-eks-cluster/cluster"
-log_stream_name = "kube-apiserver-audit-a6d77126da55ed7c1bc2c9b923dbb452"
+eks_cluster_name = "sagemaker-hyperpod-eks-cluster"
+log_group_name = f"/aws/eks/{eks_cluster_name}/cluster"
+start_datetime = "2025-05-06T18:00:00Z" # in UTC
+end_datetime = "2025-05-06T21:00:00Z" # in UTC
+object_name = "hyperpod-i-0fdb3d806306ede29"
+
+
+def datetime_to_timestamp_ms(dt):
+    if dt:
+        return int(datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ").timestamp() * 1000)
+    else:
+        return None
+
+
+class CwLogsGroup:
+
+    def __init__(self, region_name, log_group_name):
+        self.region_name = region_name
+        self.log_group_name = log_group_name
+
+    def list_streams_all(self, pattern="*", start_datetime=None, end_datetime=None):
+
+        start_timestamp = datetime_to_timestamp_ms(start_datetime)
+        end_timestamp = datetime_to_timestamp_ms(end_datetime)
+
+        logs_client = boto3.client("logs", region_name=self.region_name)
+
+        streams = []
+        next_token = None
+        while True:
+
+            params = {
+                "logGroupName" : self.log_group_name,
+                "limit" : 5,
+            }
+
+            if next_token:
+                params["nextToken"] = next_token
+
+            try:
+                response = logs_client.describe_log_streams(**params)
+            except logs_client.exceptions.ResourceNotFoundException as e:
+                print( f"Log group [{self.log_group_name}] not found" )
+                sys.exit(1)
+            
+            for stream in response["logStreams"]:
+                if not fnmatch.fnmatch(stream["logStreamName"], pattern):
+                    continue
+
+                if end_timestamp and stream["firstEventTimestamp"] > end_timestamp:
+                    continue
+
+                if start_timestamp and stream["lastEventTimestamp"] < start_timestamp:
+                    continue
+
+                streams.append(stream)
+
+            if "nextToken" in response and response["nextToken"]:
+                next_token = response["nextToken"]
+                continue
+            
+            break
+
+        return streams        
+
 
 class CwLogsStream:
 
@@ -16,7 +81,10 @@ class CwLogsStream:
         self.log_group_name = log_group_name
         self.log_stream_name = log_stream_name
 
-    def get_events(self, start_time):
+    def iter_events_all(self, start_datetime=None, end_datetime=None):
+
+        start_timestamp = datetime_to_timestamp_ms(start_datetime)
+        end_timestamp = datetime_to_timestamp_ms(end_datetime)
 
         logs_client = boto3.client("logs", region_name=self.region_name)
 
@@ -27,28 +95,34 @@ class CwLogsStream:
                 "logGroupName" : self.log_group_name,
                 "logStreamName" : self.log_stream_name,
                 "startFromHead" : True,
-                "limit" : 1000,
+                #"limit" : 100,
             }
+
+            if start_timestamp:
+                params["startTime"] = start_timestamp
+
+            if end_timestamp:
+                params["endTime"] = end_timestamp
 
             if next_token:
                 params["nextToken"] = next_token
-            else:
-                params["startTime"] = start_time
+                del params["startTime"]
 
-            #print("Calling get_log_events:", params)
+            # print("Calling get_log_events:", params)
 
             try:
                 response = logs_client.get_log_events( **params )
             except logs_client.exceptions.ResourceNotFoundException as e:
                 print( "Log group or stream not found [ %s, %s ]" % (self.log_group_name, self.log_stream_name) )
-                return
+                sys.exit(1)
+
+            # print("Num events:", len(response["events"]))
+
+            # if len(response["events"]):
+            #     print("Timestamp:", response["events"][0]["timestamp"])
 
             for event in response["events"]:
-
-                if start_time > event["timestamp"]:
-                    continue
-
-                self.on_event(event)
+                yield event
 
             assert "nextForwardToken" in response, "nextForwardToken not found"
 
@@ -57,72 +131,90 @@ class CwLogsStream:
             else:
                 break
 
-    def on_event(self, event):
-        message = event["message"]
-        message = message.replace( "\0", "\\0" )
-        print( message )
+            print(".", end="", flush=True)
 
 
-class NodeLabelUpdateEvents(CwLogsStream):
+def print_json(d):
 
-    def __init__(self, region_name, log_group_name, log_stream_name):
+    print("")
+    print(json.dumps(d, indent=2))
+    print("")
+    print("---")
 
-        super().__init__(region_name, log_group_name, log_stream_name)
-        self.verbs = set()
 
-    def on_event(self, event):
-        message = event["message"]
-        try:
-            d = json.loads(message)
-        except json.decoder.JSONDecodeError as e:
-            if print_malformed_json:
-                #print(e, ":", message)
-                pass
-            return
-        
-        try:
-            verb = d["verb"]
-        except (TypeError, KeyError) as e:
-            verb = None
-
-        self.verbs.add(verb)
-
-        # process only update and patch operations
-        if verb in {'update', 'patch'}:
-            pass
-        elif verb in {'watch', 'list', 'create', 'get'}:
-            return
-        else:
-            assert False, f"Unknown verb {verb}"
-
-        try:
-            object_name = d["objectRef"]["name"]
-        except KeyError:
-            object_name = None
-
-        # process only operations against specific node
-        if object_name != "hyperpod-i-0fdb3d806306ede29":
-            return
-        
-        labels_updated = False
-        if "requestObject" in d:
-            if "metadata" in d["requestObject"]:
-                if "labels" in d["requestObject"]["metadata"]:
-                    labels_updated = True
-
-        if labels_updated:
-            print(json.dumps(d, indent=2))
-            print("")
-            print("---")
-            print("")
-            
 def main():
 
-    node_label_events = NodeLabelUpdateEvents(region_name, log_group_name, log_stream_name)
+    log_group = CwLogsGroup(region_name, log_group_name)
     
-    #start_time = int((time.time() - 24 * 60 * 60) * 1000) # 24 hours
-    start_time = int((time.time() - 1 * 60 * 60) * 1000) # 1 hour
+    streams = log_group.list_streams_all(
+        pattern = "kube-apiserver-audit-*",
+        start_datetime = start_datetime,
+        end_datetime = end_datetime,
+        )
+    
+    for stream in streams:
 
-    node_label_events.get_events( start_time = start_time )
+        log_stream_name = stream["logStreamName"]
+
+        print("")
+        print( f"Processing log stream {log_stream_name}")
+        print("")
+
+        node_label_events = CwLogsStream(region_name, log_group_name, log_stream_name)
+        
+        for event in node_label_events.iter_events_all(
+                start_datetime = start_datetime,
+                end_datetime = end_datetime,
+            ):
+
+            message = event["message"]
+
+            # Use regex to check object name before parsing entire message as a JSON string,
+            # because long messages are truncated at 256KB. 
+            re_result = re.search(r'"objectRef":(\{[^}]+\})', message)
+            if re_result:
+                object_ref = json.loads(re_result.group(1))
+                if "name" not in object_ref or object_ref["name"] != object_name:
+                    continue
+            else:
+                continue
+
+            try:
+                d = json.loads(message)
+            except json.decoder.JSONDecodeError as e:
+                print(e, ":", message)
+                continue
+            
+            # Skip unexpected data
+            if not isinstance(d, dict) or "kind" not in d or d["kind"] != "Event":
+                continue
+
+            try:
+                verb = d["verb"]
+            except (TypeError, KeyError) as e:
+                verb = None
+
+            # process only update and patch operations
+            if verb in {'update', 'patch'}:
+                pass
+            elif verb in {'watch', 'list', 'create', 'delete', 'get', 'post'}:
+                continue
+            else:
+                print_json(d)
+                assert False, f"Unknown verb {verb}"
+
+            labels_updated = False
+            if "requestObject" in d:
+                if "metadata" in d["requestObject"]:
+                    if "labels" in d["requestObject"]["metadata"]:
+                        labels_updated = True
+
+            if labels_updated:
+                print_json(d)
+
+
+    print("")
+    print( f"Finished processing all streams")
+    print("")
 
 main()
