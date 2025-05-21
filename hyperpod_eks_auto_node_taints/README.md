17de30f6aee1c0b78b17023994d2cbe53ea0ec3c
Tomonori Shimomura <shimomut@amazon.com>
2025-05-21 20:58:44 +0000

    Experimenting Mutating Admission Webhook to achieve automatic node taints

---
+++ hyperpod_eks_auto_node_taints/README.md
@@ -0,0 +1,104 @@
+## How to set up Validating Admission Webhook for finer grain access control
+
+
+1. Generate certificate and key for the webhook
+
+    ``` bash
+    $ openssl genrsa 2048 > tls.key
+    $ openssl req -new -key tls.key -out tls_server.csr
+
+    You are about to be asked to enter information that will be incorporated
+    into your certificate request.
+    What you are about to enter is what is called a Distinguished Name or a DN.
+    There are quite a few fields but you can leave some blank
+    For some fields there will be a default value,
+    If you enter '.', the field will be left blank.
+    -----
+    Country Name (2 letter code) [AU]:US
+    State or Province Name (full name) [Some-State]:Washington
+    Locality Name (eg, city) []:Bellevue
+    Organization Name (eg, company) [Internet Widgits Pty Ltd]:Amazon
+    Organizational Unit Name (eg, section) []:SageMaker
+    Common Name (e.g. server FQDN or YOUR name) []:mutating-webhook.auto-node-taints-test.svc.cluster.local
+    Email Address []:shimomut@amazon.com
+
+    Please enter the following 'extra' attributes
+    to be sent with your certificate request
+    A challenge password []:
+    An optional company name []:
+
+    $ echo "subjectAltName = DNS:mutating-webhook.auto-node-taints-test.svc, DNS:mutating-webhook.auto-node-taints-test.svc.cluster.local" > san.txt
+    $ openssl x509 -req -sha256 -days 365 -in tls_server.csr -signkey tls.key -out tls.crt -extfile san.txt
+    ```
+
+    Note: For the SAN (X509v3 Subject Alternative Name), use following format: `{service-name}.{namespace-name}.svc.cluster.local`
+
+
+1. (Optional) Test the webhook locally
+
+    In terminal A,
+    ``` bash
+    python3 webhook.py
+    ```
+
+    In terminal B,
+    ``` bash
+    curl -s -k -H 'Content-Type: application/json' -XPOST https://localhost:8443/mutate -d @./sample-request.json
+    ```
+
+1. Add the cert and key as a Secret.
+
+    ``` bash
+    kubectl create namespace auto-node-taints-test
+    kubectl create secret tls mutating-webhook-secret --key certs/tls.key --cert certs/tls.crt -n auto-node-taints-test
+    ```
+
+    Cerify it exists.
+
+    ```bash
+    kubectl describe secret mutating-webhook-secret -n auto-node-taints-test
+    ```
+
+1. Build the image for webhook, push it to ECR, and deploy it.
+
+    ``` bash
+    make build
+    make login
+    make tag
+    make push
+    make deploy-webhook
+    ```
+
+    Confirm the webhook Pod exists and running.
+
+    ```bash
+    kubectl get pods -n auto-node-taints-test
+    ```
+
+
+1. Edit `mutating_webhook_config.yaml`
+
+    `caBundle` field has to be updated with the base64 expression of the certificate file. You can get base64 expression of the certificate by following command:
+
+    ``` bash
+    base64 -w 0 certs/tls.crt
+    ```
+
+1. Depliy the Webhook config.
+
+    ``` bash
+    kubectl apply -f mutating_webhook_config.yaml
+    ```
+
+1. Watch log from the webhook.
+
+    ``` bash
+    kubectl logs -f -l app=mutating-webhook -n auto-node-taints-test
+    ```
+
+1. Create / delete resources to test the webhook
+
+    ```
+	kubectl apply -f hello.yaml
+	kubectl delete -f hello.yaml
+    ```
---
+++ hyperpod_eks_auto_node_taints/webhook.py
@@ -0,0 +1,188 @@
+import os
+import ssl
+import json
+import base64
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+
+
+if os.path.exists("./certs"):
+    cert_dirname = "./certs"
+else:
+    cert_dirname = "/certs"
+
+
+class APIHandler(BaseHTTPRequestHandler):
+
+    def _send_response(self, data):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+    def _read_post_data(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        return json.loads(post_data.decode())
+
+    # def _validate(self, req):
+
+    #     kind = req["kind"]
+    #     operation = req["operation"]
+    #     api_invoker_username = req["userInfo"]["username"]
+    #     api_invoker_groups = req["userInfo"]["groups"]
+
+    #     print(f"Operation: {operation}, Kind: {kind}")
+
+    #     # Allow unknown kinds
+    #     if kind["kind"] not in ["Pod", "Deployment"]:
+    #         print("Ignoring unknown kinds:", kind["kind"])
+    #         return True, 200, ""
+
+    #     # Allow all groups that start with "system:" prefix (except for system:authenticated).
+    #     # Allow "dynamic-admission:admin" group.
+    #     for group in api_invoker_groups:
+    #         if group in ["system:authenticated"]:
+    #             continue
+    #         if group.startswith("system:") or group == "dynamic-admission:admin":
+    #             print(f"Allowing by group: {group}")
+    #             return True, 200, ""
+
+    #     # Allow all resource creation operations.
+    #     if operation in ["CREATE"]:
+    #         print(f"Allowing operation {operation}")
+    #         return True, 200, ""
+
+    #     # Get target object information
+    #     if operation in ["UPDATE", "DELETE", "CONNECT"]:
+    #         obj = req["oldObject"]
+    #     elif operation in ["CONNECT"]:
+    #         obj = req["object"]
+
+    #     # Get owner information from labels
+    #     try:
+    #         obj_owner = obj["metadata"]["labels"]["dynamic-admission-owner"]
+    #     except KeyError:
+    #         obj_owner = None
+
+    #     # If owner information is missing in the object, allow everything
+    #     if obj_owner is None:
+    #         print(f"Allowing {operation} operation to resources without dynamic admission owner")
+    #         return True, 200, ""
+
+    #     if obj_owner != api_invoker_username:
+    #         return False, 403, f"Forbidden access to resource owned by different user (owner:{obj_owner} != you:{api_invoker_username})"
+
+    #     print(f"Allowing {operation} operation to resources owned by the API invoker ({api_invoker_username})")
+    #     return True, 200, ""
+
+    def do_POST(self):
+
+        parsed_url = urlparse(self.path)
+        api_path = parsed_url.path
+
+        if api_path == '/mutate':
+            try:
+                post_data = self._read_post_data()
+
+                # print("Request:")
+                # print(json.dumps(post_data, indent=2))
+
+                request = post_data["request"]
+                request_id = request["uid"]
+
+                # is_allowed, code, message = self._validate(post_data["request"])
+
+                # if is_allowed:
+                #     response = {
+                #         "apiVersion": "admission.k8s.io/v1",
+                #         "kind": "AdmissionReview",
+                #         "response": {
+                #             "uid": request_id,
+                #             "allowed": True
+                #         }
+                #     }
+                # else:
+                #     response = {
+                #         "apiVersion": "admission.k8s.io/v1",
+                #         "kind": "AdmissionReview",
+                #         "response": {
+                #             "uid": request_id,
+                #             "allowed": False,
+                #             "status": {
+                #                 "code": code,
+                #                 "message": message
+                #             }
+                #         }
+                #     }
+
+                response = {
+                    "apiVersion": "admission.k8s.io/v1",
+                    "kind": "AdmissionReview",
+                    "response": {
+                        "uid": request_id,
+                        "allowed": True
+                    }
+                }
+
+                if request["kind"]["kind"] == "Pod" and request["operation"] == "CREATE":
+
+                    print("Request:")
+                    print(json.dumps(post_data, indent=2))
+
+                    patch = [
+                        {
+                            "op": "add",
+                            "path": "/metadata/labels/injected-by",
+                            "value": "mutation-webhook"
+                        }
+                    ]
+                    
+                    # Base64 encode the patch
+                    patch_bytes = json.dumps(patch).encode('utf-8')
+                    base64_patch = base64.b64encode(patch_bytes).decode('utf-8')
+                    
+                    # Add the patch to the response
+                    response["response"]["patchType"] = "JSONPatch"
+                    response["response"]["patch"] = base64_patch                    
+
+                    print("Response:")
+                    print(json.dumps(response, indent=2))
+
+                self._send_response(response)
+
+            except json.JSONDecodeError:
+                self.send_response(400)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({
+                    'error': 'Invalid JSON data',
+                    'status': 'error'
+                }).encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+        print("---")
+        print("")
+
+
+def run_server(host='0.0.0.0', port=8443):
+    server_address = (host, port)
+    httpd = HTTPServer(server_address, APIHandler)
+    
+    # SSL context configuration
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(
+        certfile = os.path.join(cert_dirname, "tls.crt"),
+        keyfile = os.path.join(cert_dirname, "tls.key"),
+    )
+    
+    # Wrap the socket with SSL
+    httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
+    
+    print(f'Starting secure server on {host}:{port}')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_server()
