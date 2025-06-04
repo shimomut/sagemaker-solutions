2aca6cace77cc5c13ff8f35a7d7f10194415d267
Tomonori Shimomura <shimomut@amazon.com>
2025-06-04 19:30:04 +0000

    Working on service installation

--- _experiments/hyperpod_imds_proxy/imds-proxy-iptables.sh
+++ _experiments/hyperpod_imds_proxy/imds-proxy-iptables.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 iptables -t nat -D OUTPUT -d 169.254.169.254 -p tcp --dport 80 -j DNAT --to-destination 169.254.0.2:9081
-iptables -t nat -D OUTPUT -d 169.254.169.254 -p tcp --dport 80 -m owner --uid-owner 0 -j DNAT --to-destination 169.254.0.2:9081
-iptables -t nat -D OUTPUT -d 169.254.169.254 -p tcp --dport 80 -m owner ! --uid-owner 0 -j DNAT --to-destination 127.0.0.1:8080
-
 iptables -t nat -I OUTPUT 1 -d 169.254.169.254 -p tcp --dport 80 -m owner --uid-owner 0 -j DNAT --to-destination 169.254.0.2:9081
 iptables -t nat -I OUTPUT 1 -d 169.254.169.254 -p tcp --dport 80 -m owner ! --uid-owner 0 -j DNAT --to-destination 127.0.0.1:8080
-
-iptables -t nat -L OUTPUT --line-numbers
+iptables -A OUTPUT -d 169.254.0.2 -p tcp --dport 9081 -m owner ! --uid-owner 0 -j DROP
