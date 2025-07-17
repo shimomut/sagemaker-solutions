f99660e0242c5fc9be3f08efc25718aa6c66e4f8
Tomonori Shimomura <shimomut@amazon.com>
2025-07-17 20:30:45 +0000

    Testing using FSX

--- _experiments/hyperpod_slurm_shutdown_action/shutdown-action.py
+++ _experiments/hyperpod_slurm_shutdown_action/shutdown-action.py
@@ -8,16 +8,20 @@ os.makedirs(output_dir, exist_ok=True)
 
 t0 = datetime.datetime.now()
 
+i = 0
 while True:
     t1 = datetime.datetime.now()
     if t1 - t0 > datetime.timedelta(minutes=3):
         break
 
-    filename = t1.strftime("%Y%m%d_%H%M%S.txt")
+    filename = t1.strftime(f"%Y%m%d_%H%M%S.txt")
     filepath = os.path.join(output_dir, filename)
 
     print("Writing", filepath)
     with open(filepath, "w") as fd:
         fd.close()
 
-    time.sleep(5)
+    time.sleep(1)
+
+    i+=1
+
