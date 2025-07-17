5383aa6733ec5dd28fef225909986c6e6a03e2a2
Tomonori Shimomura <shimomut@amazon.com>
2025-07-17 21:02:14 +0000

    Use logging instead creating many files

--- _experiments/hyperpod_slurm_shutdown_action/shutdown-action.py
+++ _experiments/hyperpod_slurm_shutdown_action/shutdown-action.py
@@ -1,27 +1,45 @@
 import os
 import time
 import datetime
+import logging
+import traceback
 
-output_dir = "/fsx/ubuntu/shutdown-action-data"
+output_dir = "/fsx/ubuntu/shutdown-action-logs"
 
 os.makedirs(output_dir, exist_ok=True)
 
 t0 = datetime.datetime.now()
 
-i = 0
-while True:
-    t1 = datetime.datetime.now()
-    if t1 - t0 > datetime.timedelta(minutes=3):
-        break
+logger = logging.getLogger(__name__)
 
-    filename = t1.strftime(f"%Y%m%d_%H%M%S.txt")
-    filepath = os.path.join(output_dir, filename)
+log_filename = t0.strftime(f"%Y%m%d_%H%M%S.log")
+log_filepath = os.path.join(output_dir, log_filename)
 
-    print("Writing", filepath)
-    with open(filepath, "w") as fd:
-        fd.close()
+logging.basicConfig(filename=log_filepath, level=logging.INFO)
 
-    time.sleep(1)
 
-    i+=1
+def main():
 
+    i = 0
+    while True:
+        t1 = datetime.datetime.now()
+
+        elapsed_time = t1 - t0
+
+        logger.info(f"Elapsed time: {elapsed_time}")
+
+        if elapsed_time > datetime.timedelta(minutes=3):
+            break
+
+        time.sleep(1)
+
+        i+=1
+
+try:
+    main()
+
+except Exception as e:
+    logger.error(f"Error: {str(e)}")
+    logger.error("Stack trace: %s", traceback.format_exc())
+
+logger.info("Finished")
